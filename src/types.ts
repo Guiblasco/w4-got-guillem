1eb3a8b0d6fea044604d96641147b1b1ea95d0a5
Guillem Blasco <gowet92@gmail.com>
2024-07-30 20:02:56 +0200

    Create fighter interface

---
+++ src/types.ts
@@ -0,0 +1,18 @@
+export type Character = {
+  name: string;
+  surname: string;
+  age: number;
+  state: "alive" | "dead";
+  die: () => void;
+  talk: () => string;
+};
+
+type King = {
+  yearsOfReign: number;
+  talk: () => "Vais a morir todos";
+};
+
+type Fighter = {
+  weapon: string;
+  skill: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+};
