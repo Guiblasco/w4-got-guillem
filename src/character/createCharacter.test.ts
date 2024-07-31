7129f449d4c6e695e829e49a88e88226ff81a89f
Guillem Blasco <gowet92@gmail.com>
2024-07-31 22:29:49 +0200

    Add createFighter test

--- src/character/createCharacter.test.ts
+++ src/character/createCharacter.test.ts
@@ -1,20 +1,20 @@
-import Character from "../character/types";
 import createCharacter from "./createCharacter";
 
 describe("Given the function createCharacter", () => {
   describe("When it recives the values {'Jaime', 'Lannister', '23'}", () => {
     test("Then it sohuld return an object type Character with properties name:'Jaime', surname: 'Lannister', age: '23'", () => {
       const characterSpects = { name: "Jaime", surname: "Lannister", age: 23 };
-      const newCharacter = createCharacter(
-        characterSpects.name,
-        characterSpects.surname,
-        characterSpects.age,
-      );
       const expectedJaime = {
         name: "Jaime",
         surname: "Lannister",
         age: 23,
       };
+
+      const newCharacter = createCharacter(
+        characterSpects.name,
+        characterSpects.surname,
+        characterSpects.age,
+      );
 
       expect(newCharacter.name).toBe(expectedJaime.name);
       expect(newCharacter.surname).toBe(expectedJaime.surname);
