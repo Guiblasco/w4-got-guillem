95cb2d185065076ecc7b278eb21d4b9690487e60
Guillem Blasco <gowet92@gmail.com>
2024-08-01 19:20:05 +0200

    Add function characters talk

--- src/adviser/type.ts
+++
@@ -1,8 +0,0 @@
-import type Character from "../character/types";
-
-export interface Adviser extends Character {
-  characterAdvises: Character;
-  talk: () => "No sé por qué, pero creo que voy a morir pronto";
-}
-
-export default Adviser;
